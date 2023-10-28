2179cef101f7068d07db920766d77d1ad756cb2b
kyleezzy <magharikyle@gmail.com>
2023-10-28 01:03:24 -0700

    Added sounds to gui's and entering elevator Added a start game screen with gui and blur effects

--- src/Lobby/client/Elevator.client.lua
+++ src/Lobby/client/Elevator.client.lua
@@ -1,12 +1,16 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-
+local SoundService = game:GetService("SoundService")
 -- References
 local RemoteEvents = ReplicatedStorage:WaitForChild("RemoteEvents")
 local RemoteEventsElevator = RemoteEvents:WaitForChild("Elevator")
 local elevatorEnterLeaveEvent = RemoteEventsElevator:WaitForChild("EnterLeave")
 local elevatorTeleportEvent = RemoteEventsElevator:WaitForChild("Teleporting")
+
+
+local buttonClick = SoundService.ButtonPress
+local enterElevator = SoundService.Woosh
 
 local Player = game.Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui") 
@@ -18,6 +22,7 @@ local exitButtonGui = PlayerGui:WaitForChild("exitButtonGui")
 
 ----------------------- FUNCTIONS -----------------------
 elevatorEnterLeaveEvent.OnClientEvent:Connect(function(elevator)
+    enterElevator:Play()
     -- set the exit gui button visible and change camera to view from outside
     exitButtonGui.TextButton.Visible = true
     camera.CameraType = Enum.CameraType.Scriptable
@@ -30,11 +35,13 @@ end)
     
 
 exitButtonGui.TextButton.Activated:Connect(function()
+    elevatorEnterLeaveEvent:FireServer()
+    task.wait(.04)
+    buttonClick:Play()
     -- set camera back to normal, and teleport the player from server side
     exitButtonGui.TextButton.Visible = false
-    camera.CameraType = Enum.CameraType.Custom
     camera.CameraSubject = Player.Character.Humanoid
-    elevatorEnterLeaveEvent:FireServer()
+    camera.CameraType = Enum.CameraType.Custom
 end)
 
 
--- src/Lobby/client/Setup.client.lua
+++ src/Lobby/client/Setup.client.lua
@@ -1,13 +1,25 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local SoundService = game:GetService("SoundService")
+local Lighting = game:GetService("Lighting")
 
 
 -- References
+local Player = game.Players.LocalPlayer
+
+local PlayerGui = Player:WaitForChild("PlayerGui") 
+local StartGame = PlayerGui:WaitForChild("StartGame")
+local blurGui = StartGame:WaitForChild("ScreenGui")
+
+local buttonClick = SoundService.ButtonPress
+
+local camera = workspace.CurrentCamera
+local introPart = workspace.Map.StartScreen
 
 -- Variables
-local Player = game.Players.LocalPlayer
-local PlayerGui = Player:WaitForChild("PlayerGui") 
+local saveWalkSpeed = 16
+
+
 
 
 ------------------------------------------- MAIN -------------------------------------------
@@ -15,3 +27,37 @@ local PlayerGui = Player:WaitForChild("PlayerGui")
 SoundService.LobbyBM:Play()
 
 -- GUI's here
+blurGui.Enabled = true
+blurGui.TextButton.Visible = true
+-- load camera for ariel view
+camera.CameraType = Enum.CameraType.Scriptable
+camera.CFrame = introPart.CFrame
+
+-- Blur temporary effect here
+local blur = Instance.new("BlurEffect")
+blur.Parent = Lighting
+blur.Size = 10
+-- grab previous walkspeed
+local playerHumanoid = Player.Character:WaitForChild("Humanoid")
+
+if playerHumanoid then
+    saveWalkSpeed = playerHumanoid.WalkSpeed
+end
+playerHumanoid.WalkSpeed = 0
+
+blurGui.TextButton.Activated:Connect(function()
+    buttonClick:Play()
+    -- short transition blur without tweening
+    for i=10, 1, -1 do
+        blur.Size = i
+        task.wait(.05)
+    end
+    -- Destroy blur as we do not need it
+    blur:Destroy()
+    blurGui.Enabled = false
+    -- revert camera and movement
+    camera.CameraType = Enum.CameraType.Custom
+    camera.CameraSubject = Player.Character.Humanoid
+    playerHumanoid.WalkSpeed = saveWalkSpeed 
+    introPart:Destroy()
+end)
